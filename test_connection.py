4d7a3883947ad52698366fa40d033e6706a27334
Louis <louis.dupand@gmail.com>
2024-02-25 03:47:34 +0100

    first version of the connection solving first cache problem

--- test_connection.py
+++ test_connection.py
@@ -4,14 +4,12 @@ import hashlib
 
 # Update Google Sheets with the new vendor data
 #conn.update(worksheet="Example 1", data=data)
-
+conn = st.connection("gsheets", type=GSheetsConnection)
+data = conn.read(worksheet="Example 1",usecols=range(3),  # specify columns which you want to get, comment this out to get all columns
+        ttl = 20)
+data = data.dropna(how="all")
 
 def check_credentials(username, password):
-
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    data = conn.read(worksheet="Example 1",usecols=range(3),  # specify columns which you want to get, comment this out to get all columns
-            ttl = 20)
-    data = data.dropna(how="all")
 
     def encrypt_password(password):
         return hashlib.sha256(password.encode()).hexdigest()
